3780d7a2192839d6e0d07610c025ec4164ebcac4
Tymisko <jasiekurbas123@gmail.com>
2020-11-23 01:57:34 +0100

    'database'

--- to_base.php
+++ to_base.php
@@ -1,9 +1,9 @@
 <?php
 // Enter your Host, username, password, database below.
-$server = "localhost";
-$base_login = "root";
-$base_password = "";
-$base = "register";
+$server = "ec2-54-228-250-82.eu-west-1.compute.amazonaws.com";
+$base_login = "ksguijihqgxdaq";
+$base_password = "4a8fa7e8c0560ef05e6d5f409302e02aa7bb2e129b2883e807d8c3b8cf90d405";
+$base = "d6o80eb5v0jt2b";
 
 $to_base = mysqli_connect($server,$base_login,$base_password,$base);
 // Check connection
